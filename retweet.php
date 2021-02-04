5c0c8b425b2ce597cab86e1660fdea088684fd4e
Slvstar <emad.storm@gmail.com>
2021-02-04 17:22:16 +0100

    The retweet Function! under Construction

---
+++ retweet.php
@@ -0,0 +1,14 @@
+<?php
+// Including The DB Connetction file
+include('connect.php');
+
+
+// The Retweet function! 
+$userid = '2';
+$tweet = $_POST['content'];
+$query = "INSERT INTO tweets (user_id, tweet_content) VALUES('$userid', '$tweet')";
+if (isset($retweet)) {
+    $sql = "INSERT INTO tweets(user_id, tweet_content) VALUES('$userid', '$tweet'";
+    mysqli_query($db, $query);
+    $retweet = $_POST['retweet'];
+}
