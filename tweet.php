d339124db75cf1a8a46a3f55f9ecddfdeb4f112b
Slvstar <emad.storm@gmail.com>
2021-02-04 17:33:05 +0100

    Fixing some isuues!

--- tweet.php
+++ tweet.php
@@ -3,6 +3,7 @@ include('connect.php');
 
 $userid = '1';
 $tweet = $_POST['content'];
+
 $query = "INSERT INTO tweets (user_id, tweet_content) VALUES('$userid', '$tweet')";
 mysqli_query($db, $query);
 
