d5e85b28f88ff72d553747604a55a4bbb58f6b20
Slvstar <emad.storm@gmail.com>
2021-02-04 17:10:11 +0100

    Fixing the errors in the Index page that is preventing from opening the Navmenu

--- page-content1.php
+++ page-content1.php
@@ -1,20 +1,34 @@
 <?php
 /*
 The main page content
-*/
-include("header.php"); // Including the header file 
+*/ include("connect.php"); // Include the DB connection
 ?>
 
-<div class="the-container">
-    <div class="navbar ">
-        <div class="menu">
-            <h3 class="logo">Coding<span>Challenge</span></h3>
-            <div class="hamburger-menu">
-                <div class="bar"></div>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Challenge</title>
+    <!-- Bootstrap, Jquery and my custom CSS files -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+</head>
+
+<body>
+    <div class="the-container">
+        <div class="navbar ">
+            <div class="menu">
+                <h3 class="logo">Coding<span>Challenge</span></h3>
+                <div class="hamburger-menu">
+                    <div class="bar"></div>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="main-container">
-        <div class="main">
-            <section class="page-content">
-                <div class="overlay">
+        <div class="main-container">
+            <div class="main">
+                <section class="page-content">
+                    <div class="overlay">
